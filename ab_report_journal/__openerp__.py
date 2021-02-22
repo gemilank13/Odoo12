81108b6317f3a063150d3795cb0a5695a58c9809
gemilank13 <39510051+gemilank13@users.noreply.github.com>
2021-02-22 12:40:56 +0700

    Add files via upload

---
+++ ab_report_journal/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Geger Gemilank
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "EDI Indonesia - Report Journal Entries",
+    "version": "8.0.1.0.0",
+    "category": "localization",
+    "website": "https://edi-indonesia.co.id",
+    "author": "Geger - PT EDI Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "account","base","report"
+    ],
+    "data": ["views/report.xml","views/report_journal.xml"],
+}
+
