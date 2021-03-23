ee5e2efc4dc5b2be556955ffd9191524260ae506
gemilank13 <39510051+gemilank13@users.noreply.github.com>
2021-03-24 01:23:10 +0700

    Add files via upload

---
+++ ab_inherit_stock_card/models/inherit_move.py
@@ -0,0 +1,12 @@
+from odoo import models, fields, api
+
+class InheritStockMove(models.Model):
+    _inherit='stock.move'
+
+    ab_project = fields.Char(string="Project", compute='_get_value')
+
+    @api.model
+    def _get_value(self):
+        for rec in self:
+            field1 = self.env ['mrp.production'].search([('name','=',rec.origin)])
+            rec.ab_project = field1.ab_project
