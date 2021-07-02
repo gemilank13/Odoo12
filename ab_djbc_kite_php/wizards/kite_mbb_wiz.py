22e0bd0f1a462cc98afbf7b23f9463a5ed6a9577
gemilank13 <39510051+gemilank13@users.noreply.github.com>
2021-07-02 07:35:03 +0700

    Add files via upload

---
+++ ab_djbc_kite_php/wizards/kite_mbb_wiz.py
@@ -0,0 +1,26 @@
+from odoo import models, fields, api
+
+
+class DJBCKiteMbbWiz(models.TransientModel):
+    _name = "djbc.kite.mbb.wizard"
+    date_start = fields.Date(string='Date Start')
+    date_end = fields.Date(string='Date End')
+
+    @api.multi
+    def generate_laporan(self):
+        cr = self.env.cr
+        cr.execute("select djbc_kite_mbb(%s,%s)",(self.date_start, self.date_end))
+        waction = self.env.ref("ab_djbc_kite_php.""kite_mbb_action")
+        result = waction.read()[0]
+        return result
+
+    @api.onchange('date_end')
+    @api.multi
+    def onchange_date(self):
+        res={}
+        if self.date_start>self.date_end:
+            res = {'warning':{
+                'title':('Warning'),
+                'message':('Tanggal Akhir Lebih Kecil Dari Tanggal Mulai')}}
+        if res:
+            return res
