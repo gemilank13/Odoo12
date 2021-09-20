c6c10b243440d2430c63142846e23e234ff9f7b2
gemilank13 <39510051+gemilank13@users.noreply.github.com>
2021-09-20 13:35:30 +0700

    Update inherit_journal.py

--- ab_unit_kerja/models/inherit_journal.py
+++ ab_unit_kerja/models/inherit_journal.py
@@ -3,12 +3,14 @@ from odoo import models, fields, api
 class InheritJournal(models.Model):
     _inherit='account.move'
 
+    unit_kerja = fields.Selection([ ('BOB', 'BOB'),('ACW', 'ACW'), ('SOR', 'SOR'), ('CIG', 'CIG')],'Unit Kerja',readonly=False, compute="_get_value2")
+
+
     @api.one
     @api.depends('name')
-    def _get_value(self):
+    def _get_value2(self):
+
         field1 = self.env ['account.invoice'].search([('number','=',self.name)])
-        self.unit_kerja = field1.unit_kerja
+        self.unit_kerja = field1.unit_kerja or False
 
 
-    # unit_kerja = fields.Selection([ ('BOB', 'BOB'),('ACW', 'ACW'), ('SOR', 'SOR'), ('CIG', 'CIG')],'Unit Kerja')
-    unit_kerja = fields.Char(string="Unit Kerja", compute="_get_value")
