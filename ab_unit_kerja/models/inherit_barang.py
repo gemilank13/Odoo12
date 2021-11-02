30a38e2169f7a908322a2bfb5815c24189db928b
gemilank13 <39510051+gemilank13@users.noreply.github.com>
2021-11-02 21:53:33 +0700

    Delete ab_unit_kerja directory

--- ab_unit_kerja/models/inherit_barang.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields, api
-
-class InheritProduct(models.Model):
-    _inherit='product.template'
-
-    unit_kerja = fields.Selection([ ('BOB', 'BOB'),('ACW', 'ACW'), ('SOR', 'SOR'), ('CIG', 'CIG')],'Unit Kerja')
