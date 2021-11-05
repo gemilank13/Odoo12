f4a5c45926c10bf1a5b9ba92370360eed06c26e6
gemilank13 <39510051+gemilank13@users.noreply.github.com>
2021-11-05 17:50:36 +0700

    Delete ab_unit_kerja directory

--- ab_unit_kerja/models/inherit_journal_item.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields, api
-
-class InheritJournalItem(models.Model):
-    _inherit='account.move.line'
-
-    unit_kerja = fields.Selection([ ('ALL', 'ALL'), ('BOB', 'BOB'),('ACW', 'ACW'), ('SOR', 'SOR'), ('CIG', 'CIG')],'Unit Kerja')
