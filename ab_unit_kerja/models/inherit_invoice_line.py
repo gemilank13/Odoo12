f02b21154aa6c7609371caeb8caad3938667e044
gemilank13 <39510051+gemilank13@users.noreply.github.com>
2021-11-07 22:54:09 +0700

    Delete ab_unit_kerja directory

--- ab_unit_kerja/models/inherit_invoice_line.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields, api
-
-class InheritInvoiceLine(models.Model):
-    _inherit='account.invoice.line'
-
-    unit_kerja = fields.Selection([ ('ALL', 'ALL'), ('BOB', 'BOB'),('ACW', 'ACW'), ('SOR', 'SOR'), ('CIG', 'CIG')],'Unit Kerja')
